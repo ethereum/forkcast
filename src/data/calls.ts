d5d7f769c12919f615fe121d9b8d8213320f86b9
wolovim <3621728+wolovim@users.noreply.github.com>
2025-11-13 13:11:54 -0500

    add acdc 169

--- src/data/calls.ts
+++ src/data/calls.ts
@@ -22,6 +22,7 @@ export const protocolCalls: Call[] = [
   { type: 'acdc', date: '2025-10-02', number: '166', path: 'acdc/166' },
   { type: 'acdc', date: '2025-10-16', number: '167', path: 'acdc/167' },
   { type: 'acdc', date: '2025-10-30', number: '168', path: 'acdc/168' },
+  { type: 'acdc', date: '2025-11-13', number: '169', path: 'acdc/169' },
 
   // ACDE calls
   { type: 'acde', date: '2025-03-27', number: '208', path: 'acde/208' },

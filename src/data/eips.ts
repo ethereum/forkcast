b314cb2f45a69b663f1704cc43e3b1bdd2998571
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2025-12-18 17:20:34 -0500

    add eip type changes

---
+++ src/data/eips.ts
@@ -0,0 +1,4 @@
+import { EIP } from '../types/eip';
+import eipsDataRaw from './eips.json';
+
+export const eipsData = eipsDataRaw as EIP[];
--- src/utils/eip.ts
+++ src/utils/eip.ts
@@ -10,9 +10,11 @@ export const getInclusionStage = (eip: EIP, forkName?: string): InclusionStage =
     fork.forkName.toLowerCase() === forkName.toLowerCase()
   );
 
-  if (!forkRelationship) return 'Unknown';
+  if (!forkRelationship || !forkRelationship.statusHistory.length) return 'Unknown';
 
-  switch (forkRelationship.status) {
+  const status = forkRelationship.statusHistory[forkRelationship.statusHistory.length - 1].status;
+
+  switch (status) {
     case 'Proposed':
       return 'Proposed for Inclusion';
     case 'Considered':
@@ -23,6 +25,8 @@ export const getInclusionStage = (eip: EIP, forkName?: string): InclusionStage =
       return 'Declined for Inclusion';
     case 'Included':
       return 'Included';
+    case 'Withdrawn':
+      return 'Withdrawn';
     default:
       return 'Unknown';
   }

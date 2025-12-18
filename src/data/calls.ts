8f3253a646ec19fdd3cce2410304537c62eaa45f
wolovim <3621728+wolovim@users.noreply.github.com>
2025-12-18 14:38:55 -0500

    add acde 226

--- src/data/calls.ts
+++ src/data/calls.ts
@@ -45,6 +45,7 @@ export const protocolCalls: Call[] = [
   { type: 'acde', date: '2025-10-23', number: '223', path: 'acde/223' },
   { type: 'acde', date: '2025-11-06', number: '224', path: 'acde/224' },
   { type: 'acde', date: '2025-12-04', number: '225', path: 'acde/225' },
+  { type: 'acde', date: '2025-12-18', number: '226', path: 'acde/226' },
 
   // ACDT calls
   { type: 'acdt', date: '2025-06-16', number: '040', path: 'acdt/040' },
